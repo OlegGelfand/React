3428ee450ad40e238f1f3101fe7b969f465aadc0
OlegGelfand <57053418+OlegGelfand@users.noreply.github.com>
2020-03-30 19:36:42 -0400

    minor changes

--- react-p2/src/Components/MainPage/MainPage.js
+++ react-p2/src/Components/MainPage/MainPage.js
@@ -10,11 +10,16 @@ function MainPage(props) {
       <div className="threecars">
              <img src="https://www.tesla.com/ns_videos/hp_2019/main-hero-desktop.jpg" alt="img1" /> 
             <img src="https://www.tesla.com/ns_videos/homepage/homepage_energy_hero_desktop.jpg" alt="img2" />
-            <video width="100%" height="800" controls autoplay onload className="source">
+      </div>
+      <div>
+            <video controls autoplay onload className="source">
             <source src="https://www.tesla.com/ns_videos/accessories-hero-desktop.mp4" type="video/mp4"></source>
             </video>
+      </div>
+      <div>
             <img id="child" src="https://www.tesla.com/sites/tesla/files/curatedmedia/blue-hero-desktop.jpg" alt="img3" />
-          </div>
+      </div>
+          
       </body>
     );
   }
