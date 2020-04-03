c28e406850f7186920c929b1054c7caab391fe5c
OlegGelfand <57053418+OlegGelfand@users.noreply.github.com>
2020-04-03 12:13:06 -0400

    Updates

---
+++ precache-manifest.0dcf66876439e2fcc69a41894fbead3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43753a554270d74b1da4e6fd5463236",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "1de6b4290ad9c141e48b",
+    "url": "/React/static/css/main.7a6fe976.chunk.css"
+  },
+  {
+    "revision": "03195136195e7cd53ee0",
+    "url": "/React/static/js/2.20e25245.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.20e25245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de6b4290ad9c141e48b",
+    "url": "/React/static/js/main.9f215f22.chunk.js"
+  },
+  {
+    "revision": "d705f2ad7071d8dfbe62",
+    "url": "/React/static/js/runtime-main.16ddd4eb.js"
+  }
+]);
